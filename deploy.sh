3e5ce454fb597d3b113d16fa9b69688916ec1bd5
mcmillanator <me@stanleymcmillan.com>
2018-11-04 00:39:56 -0400

    Delete old files

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-aws s3 cp --recursive jekyll/_site s3://race-records/
--- package.sh
+++
@@ -1,4 +0,0 @@
-bucket=race-records-package
-stackName=race-records-b
-sam package --template-file template.yaml --output-template-file packaged.yaml --s3-bucket $bucket
-sam deploy --template-file packaged.yaml --stack-name $stackName --capabilities CAPABILITY_IAM --region us-east-1
