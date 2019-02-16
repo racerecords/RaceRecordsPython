0bad2530903718217aae34ad0e173c31e0b562fb
mcmillanator <me@stanleymcmillan.com>
2019-02-16 11:57:28 -0500

    Add background option to jekyll dev script

--- dev-scripts/jekyll.sh
+++ dev-scripts/jekyll.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 cd ..
 export JEKYLL_VERSION=3.8
+options='--name jekyll --rm -p 4000:4000'
 case $1 in
 	build)
 		cmd='jekyll build'
@@ -8,8 +9,12 @@ case $1 in
 	bash)
 		cmd='/bin/bash'
 	;;
+	background)
+		cmd='jekyll server 0.0.0.0'
+		options="$options -d"
+	;;
 	*)
 		cmd='jekyll server 0.0.0.0'
 	;;
 esac
-docker run --name jekyll --rm -p 4000:4000 -v jekyll-gems:/usr/local/bundle  --volume="$PWD/jekyll:/srv/jekyll" -it jekyll/jekyll:$JEKYLL_VERSION $cmd
+docker run $options -v jekyll-gems:/usr/local/bundle  --volume="$PWD/jekyll:/srv/jekyll" -it jekyll/jekyll:$JEKYLL_VERSION $cmd
